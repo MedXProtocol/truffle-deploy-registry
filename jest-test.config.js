321bc3fd981c50babdbde56e025ae03547533910
Charles Bergeron <chuck.bergeron@gmail.com>
2019-01-08 16:44:05 -0800

    * fix up code coverage

--- jest-test.config.js
+++ jest-test.config.js
@@ -21,9 +21,9 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: [
-    "lib/**/*.{js,jsx}"
-  ],
+  // collectCoverageFrom: [
+  //   "**/*.{js,jsx}"
+  // ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
